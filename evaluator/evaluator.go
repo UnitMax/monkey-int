bac6c3d6bf0d7dc943864408fd48202a2e565aee
UnitMax <105129888+UnitMax@users.noreply.github.com>
2024-01-30 21:21:14 +0100

    evaluate minus prefix operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -50,6 +50,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -65,5 +67,14 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	switch r := right.(type) {
+	case *object.Integer:
+		return &object.Integer{Value: -r.Value}
+	default:
+		return NULL
 	}
 }
