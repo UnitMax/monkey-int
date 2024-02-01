c4562a5f7513e14e9f40b2d7cdafb922225fb323
UnitMax <105129888+UnitMax@users.noreply.github.com>
2024-02-01 23:10:54 +0100

    eval integer comparisons

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -62,6 +62,14 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}{
 		{"false", false},
 		{"true", true},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
