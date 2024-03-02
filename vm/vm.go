39542b22a124d979c57cc6c977dca491caa0981d
UnitMax <105129888+UnitMax@users.noreply.github.com>
2024-03-02 02:01:27 +0100

    stack push for VM

--- vm/vm.go
+++ vm/vm.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"fmt"
 	"monkey-int/bytecode"
 	"monkey-int/compiler"
 	"monkey-int/object"
@@ -13,7 +14,7 @@ type VM struct {
 	instructions bytecode.Instructions
 
 	stack []object.Object
-	sp    int // stackpointer
+	sp    int // stackpointer, always pointing to the next FREE slot in the stack
 }
 
 func New(myBytecode *compiler.MyBytecode) *VM {
@@ -23,4 +24,37 @@ func New(myBytecode *compiler.MyBytecode) *VM {
 		stack:        make([]object.Object, StackSize),
 		sp:           0,
 	}
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := bytecode.Opcode(vm.instructions[ip])
+
+		switch op {
+		case bytecode.OpConstant:
+			constIndex := bytecode.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("Stack overflow!")
+	}
+	vm.stack[vm.sp] = o
+	vm.sp++
+	return nil
 }
