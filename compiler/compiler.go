e9783fb48ebc915b1b7a51a4e546cecc5bb4c468
UnitMax <105129888+UnitMax@users.noreply.github.com>
2024-03-28 22:28:15 +0100

    compiler scopes

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -213,6 +213,22 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(bytecode.OpIndex)
+	case *ast.FunctionLiteral:
+		c.enterScope()
+		err := c.Compile(node.Body)
+		if err != nil {
+			return err
+		}
+		instructions := c.leaveScope()
+
+		compiledFn := &object.CompiledFunction{Instructions: instructions}
+		c.emit(bytecode.OpConstant, c.addConstant(compiledFn))
+	case *ast.ReturnStatement:
+		err := c.Compile(node.ReturnValue)
+		if err != nil {
+			return err
+		}
+		c.emit(bytecode.OpReturnValue)
 	}
 	return nil
 }
